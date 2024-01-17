a5325333ffbcce2cca9336b4d8aef67991d77774
LakshinPathak <145486751+LakshinPathak@users.noreply.github.com>
2024-01-17 09:23:17 +0530

    Add files via upload

--- public/extra.js
+++ public/extra.js
@@ -147,3 +147,93 @@ displayUserList();
 //              You are invited to our event!<br>
 //              Please let us know if you'll be able to attend by responding to this email.<br>  
 //                         We look forward to seeing you!</p>`
+
+
+
+
+
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'; 
+const GOOGLE_PRIVATE_KEY="f5e87b9500f260cfb3834365785f892775a1a97d"
+const GOOGLE_CLIENT_EMAIL = "lakshinpathak2003@gmail.com"
+const GOOGLE_PROJECT_NUMBER = "771496546384"
+const GOOGLE_CALENDAR_ID = "4a80cfaebace8d0965cfa4f5c1215249810dc0cd43372bca80a5964a1100c1a3@group.calendar.google.com"
+
+
+const jwtClient = new google.auth.JWT( 
+    GOOGLE_CLIENT_EMAIL, 
+    null, 
+    GOOGLE_PRIVATE_KEY, 
+    SCOPES 
+  ); 
+    
+  const calendar = google.calendar({ 
+    version: 'v3', 
+    project: GOOGLE_PROJECT_NUMBER, 
+    auth: jwtClient 
+  }); 
+    
+  app.get('/', (req, res) => { 
+    calendar.events.list({ 
+      calendarId: GOOGLE_CALENDAR_ID, 
+      timeMin: (new Date()).toISOString(), 
+      maxResults: 10, 
+      singleEvents: true, 
+      orderBy: 'startTime', 
+    }, (error, result) => { 
+      if (error) { 
+        res.send(JSON.stringify({ error: error })); 
+      } else { 
+        if (result.data.items.length) { 
+          res.send(JSON.stringify({ events: result.data.items })); 
+        } else { 
+          res.send(JSON.stringify({ message: 'No upcoming events found.' })); 
+        } 
+      } 
+    }); 
+  }); 
+    
+
+
+
+  app.get("/createEvent_calender",(req,res)=>{ 
+    var event = { 
+      'summary': 'My first event!', 
+      'location': 'Hyderabad,India', 
+      'description': 'First event with nodeJS!', 
+      'start': { 
+        'dateTime': '2022-01-12T09:00:00-07:00', 
+        'timeZone': 'Asia/Dhaka', 
+      }, 
+      'end': { 
+        'dateTime': '2022-01-14T17:00:00-07:00', 
+        'timeZone': 'Asia/Dhaka', 
+      }, 
+      'attendees': [], 
+      'reminders': { 
+        'useDefault': false, 
+        'overrides': [ 
+          {'method': 'email', 'minutes': 24 * 60}, 
+          {'method': 'popup', 'minutes': 10}, 
+        ], 
+      }, 
+    }; 
+      
+    const auth = new google.auth.GoogleAuth({ 
+      keyFile: '<full-path-of-JSON-file>', 
+      scopes: 'https://www.googleapis.com/auth/calendar', 
+    }); 
+    auth.getClient().then(a=>{ 
+      calendar.events.insert({ 
+        auth:a, 
+        calendarId: GOOGLE_CALENDAR_ID, 
+        resource: event, 
+      }, function(err, event) { 
+        if (err) { 
+          console.log('There was an error contacting the Calendar service: ' + err); 
+          return; 
+        } 
+        console.log('Event created: %s', event.data); 
+        res.jsonp("Event successfully created!"); 
+      }); 
+    }) 
+  })
