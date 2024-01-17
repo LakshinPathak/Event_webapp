2ab61e6474475797789ce4d3c958cc0f7f5d0d12
LakshinPathak <145486751+LakshinPathak@users.noreply.github.com>
2024-01-17 09:19:12 +0530

    Add files via upload

--- server/route.js
+++ server/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { MongoClient } = require('mongodb');
 const { exec } = require('child_process');
+const { google } = require('googleapis'); 
 
 const app = express();
 app.use(express.static('eliza-master'));
@@ -16,6 +17,106 @@ const dbName = 'Lakshin25'; // Change this to your database name
 //const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 //"mongodb://localhost:27017"
+
+
+
+
+
+const SCOPES = 'https://www.googleapis.com/auth/calendar';
+const GOOGLE_PRIVATE_KEY ="-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC5yPR8+2HhkQ74\nnL2LKcxDDRU+v/+pQJbfkdc8CEV5lRhA5rkNpbNN36wbwMerpeEfNveaKgivibox\nGiD3opP+/qxmHl24Y7aGVN63VIvHfScr6QBLOBAFf76bmYKdbX1OIpaKhswdC7ch\nfGuyunVisKLvxR1dXUbBTpxoIzEfHx7kR3jqNqtgQzVl+XB4Zu4vWjoeQqw4sKDq\nACeGT2WvHt340tdn37CoZDBj+qh24EY9PqzU0313sI3hnsmpdBxdxU0dV0Ocpams\nYfoJ6tLoSifOpsvdQwwtIBiyDk4Bz91tMhXMwko2V2TDP+t4Xv7IrMsQ9l6Vhjbd\nM/XHEihJAgMBAAECggEAAtaO5EE42mfLY+viZLeuPvtPrvDi7cnRTuKyTYtOIxCD\n2YCiJL26cqmZ6PtLHD8BTzyZOrnX/nUrACix2+aYE9tTHzo+h0cVWOb/V1X2JAWF\nmCuhOATyuwrGYCfPsX+khubgstwNToeedBIBA9l1BUgagnGEmdJczC4wayeCeW21\nw7E0HGa24PwZAe2sjyldUTCWQJTtumFoB1poyh4lL+E888L8NB8AMrxHeTMbAIUT\ny6I7M4pmkailENEMEgWsETSI7AQZrvQYSaTnkZVshW8W56knbOmcTOIShqu7MPXx\niAa+bfE5gHCQqEzBWYDzksG7y3C1eINl8eXUYVU82wKBgQDqc2MIIxa3d+/KYGko\nxnf2v6DofcRAcGBdPIzjzEg0E2UIREq84ElOGWX9NMxwZwdwzKLGc6rFCy0xLysk\niGphNjdmBRepMvebmejizQVAnyC7jmiM3LpymuJVM4XGpAxvn9L0619LTuDKWGqh\ni0SuqssLUB9YhsR+YKwUcAIxywKBgQDK3Hc4lPd688u6SFctL80Bm6TVvSvgxa31\nqi9BCO3CPmn4kOCV1f40Fpsa0BT7Fm6t9xnq9IuFZW06p/Bp6pjF98wXSA8Ywh/H\n0ihR4b2yoDMuQ7kA5GWvTrnYRZLrgnb55FB6wSvoBPrRmSMya67w8UTHFOEwcoON\nn5OzBDo7uwKBgHFXlSHC7OGHfwpkb5JB2I8WdzDOVEkeysdoVWhvQtzCH47OjR8g\ngu/ZwX+Wvdr9RBCZLXwpQP5/d7qWZhIETQFwF3ofrh+jPycfUgILQaXLCCUEJLEs\nqIptLEwqW1r7xfNU3cR/4ZLjsDLCadJtJKgYc46ljnHavTWngKlkuPrfAoGBAJmO\n42xgTymyLFA5fEwHOA70i4UD6pf9613f0l9Pa/KynjL7MBYb4434YracNpWOeH+V\nYfgZ3xc+HL3e7Dywya5pkgM5l7vtDnOK4NyCnb7+R1/JAdzsBxD57fIJUFqX6onk\nywrw+NRsGJTab+s12jom2CI+6utqc6yb1AkZtR8LAoGAKX+BDovZ9ZNLF9ySGPq/\ndmfMssAj8lDQ6TcaNvco3kErsatHBUn1z+2Iak6jlj95lM1YqvqBusvEaHR2ZLGi\nLQ1lYQihvacruxST2cDXuPvgL5jD2ACZGijIzUzqap7HcTaf2b/lnPRjd1WWl/o+\n1eJmJpG8zG5RfINeaGZMMTc=\n-----END PRIVATE KEY-----\n";
+
+
+//"-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzN1kbyerAhuKF\n9iUQAG35UF7A1CEUen2HkiHcqZEuBVYjTPgwZsqvKRliThVXoYcv/k6JhgZ1J2Q5\n9IETG0QAdpgqTqBVQl35k0faORqxfDPA3vhE8lMxEeLA2L05Wp0ooM3eHmbBRQli\nd2sxpjBCi9MHzY2KscMIfwFsVgzX+FY/CN6LqlRQShCDVPOW55yNAdOc65GDi90X\nYrAf+uTH7B9IsrjVp0o9uKj8FVlWVf/2ezZnssZcjmiUnZQxIdT4qvaueOM/CW+v\nUk174SMGuJ0R+/y9ZgicIYfR3f/OtvzR4eoXz7bdb5x2OIIMtBazkw3wHcwuGQlO\n/2m3TkwtAgMBAAECggEADgFRx9+oJkKEOkbJj64Q6g5TUUtiiGioHauYVNe1fjyx\nhhGyRYoK5dRW5P1IIrB/UYtUlN+x6n7ZEMRgP99+GXErJAc/9Vm54VKWp2GWCe+0\nVaq3CSBMywVkTIDDjNNde7rRS73hdtIP0S+W42ri05h8tdvk50VMAgJ22IIC/++J\nIJh5YrdGF/4hSGTPNkS+ASbYSG2MPezzuamRNpMlJKbK3S1Ccb7jOTyjRTDzy6Ew\n2Jt9J7Z2Ja4n7XydsbBy68SyR0L3t8BcrN7KOE3WlSmu8J5h3y6GDqJPeyS2QxuY\nJ93qN5BtwTrSrvCf5ZIig0Vhsm6foQ4N7F3q4puH4QKBgQDroKexJzwahpUqdrlr\npai4zsXA69WKmrzGBrNuGlDKKBN1tD8oULIktzvaWuvif2KAkA1991h4r7d0PXQq\npfLggt7K6BeqdWSZiG60meFQcjLnhGVKkcBYLc+IvXPz8Vip3osKCqpI1jerzg/P\nuff65azPdRvN1+5nUvwcPWOFsQKBgQDCthezJMQo+sGOX624bJyfnk0yPm2XxK9R\nHhDpF1D+hI67ueJgQhxPyCAPGB2xfk2FwiKN6o+lEUUkS1aKPmx7PBvLmu4hrvkD\n+hQ0FSgKnRtCnyR0ddsPxSbHP5eJqXDa/NVCTHIEM4bSQKyEIH944blPD1kqNv3S\nbJZ/Nd3BPQKBgHmnyA0KUdjsNaXoCJIQxQ0uDSIvYWCfxemEWi31vlKx4GI8PwOk\nrmWQiYGSwgl+dbAlMCIii/Y72z9T31+JsN3EFB21OUrUM62lqGrsrE1puOiaDeYz\nXJjqhiV2k6CWso16DlWP7h2PJM06piU9QlCzNsciEsnzu+5zBSHbfD/xAoGAec48\nCT+IzzH+/7fvDx4y5M+87x+Ko5TRL+elIIwLiQ0X1Ww1p43z9Bz3oTmu6ahfirul\nri38aoDhRhEzNAxVBMrXgImNStkQwmIhXWuWvv0FXqn7vjc0MSteVOr9O4saJIRi\nTLVR5jrtrn3y7jz+QXrD2Cd72iLIqmhvxWjHLukCgYEA0H5JrRritGjhGG5LTFID\nUYoidGUSzUdD8l4fbftdTMgHltB6mtp7W3FsH2n784LYeX/1L+0Tbp2NM4KyPio8\nhVgk4+4Gzjq+xZnW/MMkCnpUg1OaZmPXnAntJjxQAHX32J8mEP6QJCsLt2EtlmrM\nSokEQWxpoiZd6s4N416V0iQ=\n-----END PRIVATE KEY-----\n";
+//"-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC5yPR8+2HhkQ74\nnL2LKcxDDRU+v/+pQJbfkdc8CEV5lRhA5rkNpbNN36wbwMerpeEfNveaKgivibox\nGiD3opP+/qxmHl24Y7aGVN63VIvHfScr6QBLOBAFf76bmYKdbX1OIpaKhswdC7ch\nfGuyunVisKLvxR1dXUbBTpxoIzEfHx7kR3jqNqtgQzVl+XB4Zu4vWjoeQqw4sKDq\nACeGT2WvHt340tdn37CoZDBj+qh24EY9PqzU0313sI3hnsmpdBxdxU0dV0Ocpams\nYfoJ6tLoSifOpsvdQwwtIBiyDk4Bz91tMhXMwko2V2TDP+t4Xv7IrMsQ9l6Vhjbd\nM/XHEihJAgMBAAECggEAAtaO5EE42mfLY+viZLeuPvtPrvDi7cnRTuKyTYtOIxCD\n2YCiJL26cqmZ6PtLHD8BTzyZOrnX/nUrACix2+aYE9tTHzo+h0cVWOb/V1X2JAWF\nmCuhOATyuwrGYCfPsX+khubgstwNToeedBIBA9l1BUgagnGEmdJczC4wayeCeW21\nw7E0HGa24PwZAe2sjyldUTCWQJTtumFoB1poyh4lL+E888L8NB8AMrxHeTMbAIUT\ny6I7M4pmkailENEMEgWsETSI7AQZrvQYSaTnkZVshW8W56knbOmcTOIShqu7MPXx\niAa+bfE5gHCQqEzBWYDzksG7y3C1eINl8eXUYVU82wKBgQDqc2MIIxa3d+/KYGko\nxnf2v6DofcRAcGBdPIzjzEg0E2UIREq84ElOGWX9NMxwZwdwzKLGc6rFCy0xLysk\niGphNjdmBRepMvebmejizQVAnyC7jmiM3LpymuJVM4XGpAxvn9L0619LTuDKWGqh\ni0SuqssLUB9YhsR+YKwUcAIxywKBgQDK3Hc4lPd688u6SFctL80Bm6TVvSvgxa31\nqi9BCO3CPmn4kOCV1f40Fpsa0BT7Fm6t9xnq9IuFZW06p/Bp6pjF98wXSA8Ywh/H\n0ihR4b2yoDMuQ7kA5GWvTrnYRZLrgnb55FB6wSvoBPrRmSMya67w8UTHFOEwcoON\nn5OzBDo7uwKBgHFXlSHC7OGHfwpkb5JB2I8WdzDOVEkeysdoVWhvQtzCH47OjR8g\ngu/ZwX+Wvdr9RBCZLXwpQP5/d7qWZhIETQFwF3ofrh+jPycfUgILQaXLCCUEJLEs\nqIptLEwqW1r7xfNU3cR/4ZLjsDLCadJtJKgYc46ljnHavTWngKlkuPrfAoGBAJmO\n42xgTymyLFA5fEwHOA70i4UD6pf9613f0l9Pa/KynjL7MBYb4434YracNpWOeH+V\nYfgZ3xc+HL3e7Dywya5pkgM5l7vtDnOK4NyCnb7+R1/JAdzsBxD57fIJUFqX6onk\nywrw+NRsGJTab+s12jom2CI+6utqc6yb1AkZtR8LAoGAKX+BDovZ9ZNLF9ySGPq/\ndmfMssAj8lDQ6TcaNvco3kErsatHBUn1z+2Iak6jlj95lM1YqvqBusvEaHR2ZLGi\nLQ1lYQihvacruxST2cDXuPvgL5jD2ACZGijIzUzqap7HcTaf2b/lnPRjd1WWl/o+\n1eJmJpG8zG5RfINeaGZMMTc=\n-----END PRIVATE KEY-----\n";
+// "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC5yPR8+2HhkQ74\nnL2LKcxDDRU+v/+pQJbfkdc8CEV5lRhA5rkNpbNN36wbwMerpeEfNveaKgivibox\nGiD3opP+/qxmHl24Y7aGVN63VIvHfScr6QBLOBAFf76bmYKdbX1OIpaKhswdC7ch\nfGuyunVisKLvxR1dXUbBTpxoIzEfHx7kR3jqNqtgQzVl+XB4Zu4vWjoeQqw4sKDq\nACeGT2WvHt340tdn37CoZDBj+qh24EY9PqzU0313sI3hnsmpdBxdxU0dV0Ocpams\nYfoJ6tLoSifOpsvdQwwtIBiyDk4Bz91tMhXMwko2V2TDP+t4Xv7IrMsQ9l6Vhjbd\nM/XHEihJAgMBAAECggEAAtaO5EE42mfLY+viZLeuPvtPrvDi7cnRTuKyTYtOIxCD\n2YCiJL26cqmZ6PtLHD8BTzyZOrnX/nUrACix2+aYE9tTHzo+h0cVWOb/V1X2JAWF\nmCuhOATyuwrGYCfPsX+khubgstwNToeedBIBA9l1BUgagnGEmdJczC4wayeCeW21\nw7E0HGa24PwZAe2sjyldUTCWQJTtumFoB1poyh4lL+E888L8NB8AMrxHeTMbAIUT\ny6I7M4pmkailENEMEgWsETSI7AQZrvQYSaTnkZVshW8W56knbOmcTOIShqu7MPXx\niAa+bfE5gHCQqEzBWYDzksG7y3C1eINl8eXUYVU82wKBgQDqc2MIIxa3d+/KYGko\nxnf2v6DofcRAcGBdPIzjzEg0E2UIREq84ElOGWX9NMxwZwdwzKLGc6rFCy0xLysk\niGphNjdmBRepMvebmejizQVAnyC7jmiM3LpymuJVM4XGpAxvn9L0619LTuDKWGqh\ni0SuqssLUB9YhsR+YKwUcAIxywKBgQDK3Hc4lPd688u6SFctL80Bm6TVvSvgxa31\nqi9BCO3CPmn4kOCV1f40Fpsa0BT7Fm6t9xnq9IuFZW06p/Bp6pjF98wXSA8Ywh/H\n0ihR4b2yoDMuQ7kA5GWvTrnYRZLrgnb55FB6wSvoBPrRmSMya67w8UTHFOEwcoON\nn5OzBDo7uwKBgHFXlSHC7OGHfwpkb5JB2I8WdzDOVEkeysdoVWhvQtzCH47OjR8g\ngu/ZwX+Wvdr9RBCZLXwpQP5/d7qWZhIETQFwF3ofrh+jPycfUgILQaXLCCUEJLEs\nqIptLEwqW1r7xfNU3cR/4ZLjsDLCadJtJKgYc46ljnHavTWngKlkuPrfAoGBAJmO\n42xgTymyLFA5fEwHOA70i4UD6pf9613f0l9Pa/KynjL7MBYb4434YracNpWOeH+V\nYfgZ3xc+HL3e7Dywya5pkgM5l7vtDnOK4NyCnb7+R1/JAdzsBxD57fIJUFqX6onk\nywrw+NRsGJTab+s12jom2CI+6utqc6yb1AkZtR8LAoGAKX+BDovZ9ZNLF9ySGPq/\ndmfMssAj8lDQ6TcaNvco3kErsatHBUn1z+2Iak6jlj95lM1YqvqBusvEaHR2ZLGi\nLQ1lYQihvacruxST2cDXuPvgL5jD2ACZGijIzUzqap7HcTaf2b/lnPRjd1WWl/o+\n1eJmJpG8zG5RfINeaGZMMTc=".split(String.raw`\n`).join('\n');
+
+//"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC5yPR8+2HhkQ74\nnL2LKcxDDRU+v/+pQJbfkdc8CEV5lRhA5rkNpbNN36wbwMerpeEfNveaKgivibox\nGiD3opP+/qxmHl24Y7aGVN63VIvHfScr6QBLOBAFf76bmYKdbX1OIpaKhswdC7ch\nfGuyunVisKLvxR1dXUbBTpxoIzEfHx7kR3jqNqtgQzVl+XB4Zu4vWjoeQqw4sKDq\nACeGT2WvHt340tdn37CoZDBj+qh24EY9PqzU0313sI3hnsmpdBxdxU0dV0Ocpams\nYfoJ6tLoSifOpsvdQwwtIBiyDk4Bz91tMhXMwko2V2TDP+t4Xv7IrMsQ9l6Vhjbd\nM/XHEihJAgMBAAECggEAAtaO5EE42mfLY+viZLeuPvtPrvDi7cnRTuKyTYtOIxCD\n2YCiJL26cqmZ6PtLHD8BTzyZOrnX/nUrACix2+aYE9tTHzo+h0cVWOb/V1X2JAWF\nmCuhOATyuwrGYCfPsX+khubgstwNToeedBIBA9l1BUgagnGEmdJczC4wayeCeW21\nw7E0HGa24PwZAe2sjyldUTCWQJTtumFoB1poyh4lL+E888L8NB8AMrxHeTMbAIUT\ny6I7M4pmkailENEMEgWsETSI7AQZrvQYSaTnkZVshW8W56knbOmcTOIShqu7MPXx\niAa+bfE5gHCQqEzBWYDzksG7y3C1eINl8eXUYVU82wKBgQDqc2MIIxa3d+/KYGko\nxnf2v6DofcRAcGBdPIzjzEg0E2UIREq84ElOGWX9NMxwZwdwzKLGc6rFCy0xLysk\niGphNjdmBRepMvebmejizQVAnyC7jmiM3LpymuJVM4XGpAxvn9L0619LTuDKWGqh\ni0SuqssLUB9YhsR+YKwUcAIxywKBgQDK3Hc4lPd688u6SFctL80Bm6TVvSvgxa31\nqi9BCO3CPmn4kOCV1f40Fpsa0BT7Fm6t9xnq9IuFZW06p/Bp6pjF98wXSA8Ywh/H\n0ihR4b2yoDMuQ7kA5GWvTrnYRZLrgnb55FB6wSvoBPrRmSMya67w8UTHFOEwcoON\nn5OzBDo7uwKBgHFXlSHC7OGHfwpkb5JB2I8WdzDOVEkeysdoVWhvQtzCH47OjR8g\ngu/ZwX+Wvdr9RBCZLXwpQP5/d7qWZhIETQFwF3ofrh+jPycfUgILQaXLCCUEJLEs\nqIptLEwqW1r7xfNU3cR/4ZLjsDLCadJtJKgYc46ljnHavTWngKlkuPrfAoGBAJmO\n42xgTymyLFA5fEwHOA70i4UD6pf9613f0l9Pa/KynjL7MBYb4434YracNpWOeH+V\nYfgZ3xc+HL3e7Dywya5pkgM5l7vtDnOK4NyCnb7+R1/JAdzsBxD57fIJUFqX6onk\nywrw+NRsGJTab+s12jom2CI+6utqc6yb1AkZtR8LAoGAKX+BDovZ9ZNLF9ySGPq/\ndmfMssAj8lDQ6TcaNvco3kErsatHBUn1z+2Iak6jlj95lM1YqvqBusvEaHR2ZLGi\nLQ1lYQihvacruxST2cDXuPvgL5jD2ACZGijIzUzqap7HcTaf2b/lnPRjd1WWl/o+\n1eJmJpG8zG5RfINeaGZMMTc=";
+const GOOGLE_CLIENT_EMAIL = "calender-key@event-calender-411413.iam.gserviceaccount.com";
+const GOOGLE_PROJECT_NUMBER = "771496546384";
+const GOOGLE_CALENDAR_ID = "4a80cfaebace8d0965cfa4f5c1215249810dc0cd43372bca80a5964a1100c1a3@group.calendar.google.com";
+
+const jwtClient = new google.auth.JWT( 
+  GOOGLE_CLIENT_EMAIL, 
+  null, 
+  GOOGLE_PRIVATE_KEY, 
+  SCOPES 
+); 
+  
+const calendar = google.calendar({ 
+  version: 'v3', 
+  project: GOOGLE_PROJECT_NUMBER, 
+  auth: jwtClient 
+});
+
+
+
+// Endpoint to create a Google Calendar event
+router.post('/createGoogleCalendarEvent', async (req, res) => {
+  const {eventData }= req.body;
+
+  try {
+      // Authenticate with Google Calendar API
+    //  const auth = await jwtClient.authorize();
+
+    const auth = new google.auth.GoogleAuth({ 
+      keyFile: 'C:\\Users\\laksh\\Downloads\\event-calender-411413-07a49e22134c.json', 
+      scopes: 'https://www.googleapis.com/auth/calendar', 
+    }); 
+
+      // Create Google Calendar event
+      calendar.events.insert({
+          auth,
+          calendarId: GOOGLE_CALENDAR_ID,
+          resource: eventData,
+      }, (err, event) => {
+          if (err) {
+              console.error('Error creating Google Calendar Event:', err);
+              res.status(500).json({ success: false, message: 'Internal server error' });
+          } else {
+              console.log('Google Calendar Event created:', event.data);
+              res.json({ success: true, message: 'Google Calendar Event created successfully!' });
+          }
+      });
+  } catch (error) {
+      console.error('Error creating Google Calendar Event:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+
+// Endpoint to get Google Calendar events
+router.get('/getGoogleCalendarEvents', async (req, res) => {
+  try {
+      // Authenticate with Google Calendar API
+      const auth = await jwtClient.authorize();
+
+      // Fetch Google Calendar events
+      calendar.events.list({
+          auth,
+          calendarId: GOOGLE_CALENDAR_ID,
+          timeMin: (new Date()).toISOString(),
+          maxResults: 10,
+          singleEvents: true,
+          orderBy: 'startTime',
+      }, (error, result) => {
+          if (error) {
+              console.error('Error fetching Google Calendar Events:', error);
+              res.status(500).json({ success: false, message: 'Internal server error' });
+          } else {
+              if (result.data.items.length) {
+                  res.json({ success: true, events: result.data.items });
+              } else {
+                  res.json({ success: true, message: 'No upcoming events found.' });
+              }
+          }
+      });
+  } catch (error) {
+      console.error('Error fetching Google Calendar Events:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+
 
 // Define a route for handling registration requests
 router.post('/register', async (req, res) => {
